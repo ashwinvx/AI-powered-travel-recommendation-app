d3ccfb9df617806d8b23b4caa48b2406395ba455
Ashwin <ashwinvx@users.noreply.github.com>
2025-09-25 15:15:08 -0400

    Mongodb connection done

---
+++ app.js
@@ -0,0 +1,54 @@
+require('dotenv-flow').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middlewares/authMiddleware');
+const corsMiddleware = require('./cors');
+const limiter = require('./middlewares/ratelimitMiddleware');
+const ExpressError = require('./middlewares/expressError');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Connect to MongoDB
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+connectDB();
+
+// view engine
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(limiter);
+app.options('/*splat', corsMiddleware);
+app.use(corsMiddleware);
+
+//routes
+app.get('/{*any}', checkUser);
+app.get('/', (req, res) => res.status(200).send('API is running...'));
+app.use("/api/users", checkUser, authRoutes);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// general error handler
+app.use(function (req, res) {
+    res.status(404).render('404', { title: '404' });
+});
+
+module.exports = app;   
